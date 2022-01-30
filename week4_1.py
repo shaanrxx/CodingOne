bac1c8df3fdbc97d15be09fbd48da1324812e9b9
unknown <shaanr@hotmail.co.uk>
2022-01-30 17:34:10 +0000

    second commit

--- week4_1.py
+++ week4_1.py
@@ -3,9 +3,23 @@
 ## Write a program that given a list of numbers, multiply all numbers in the list. Bonus for ignoring non-number element.
 ## Example: input: [1, 2, 3, 4], output: 24
 
-myList= [1,2,3]
-result= 1
-for i in myList:
-    result = result * i
+try:
+    num1 = int(input("Enter a number: "))
+    num2 = int(input("Enter a number: "))
+    num3 = int(input("Enter a number: "))
+    num4 = int(input("Enter a number: "))
+except ValueError as e:
+    print("NOT VALID")
 
-print (result)
+list = [num1, num2, num3, num4]
+result=1
+
+def multiplyList(list) :
+     
+    # Multiply elements one by one
+    result = 1
+    for x in list:
+         result = result * x
+    return result
+
+print(multiplyList(list))
